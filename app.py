9c619085cd6b6bd2879da822c34d8b0b0166b2fc
altarh <altarh@mail.tau.ac.il>
2025-07-11 14:11:37 -0400

    added distance_metric selection option

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from sentence_transformers import SentenceTransformer
 import streamlit as st
-
-# 1. Load a pretrained Sentence Transformer model
-
+from distance_metrics import DistanceMetrics
 
 
-st.title("🦜🔗 Text similarities")
+st.title("Text similarities")
 first_sentence = st.text_input("First sentence", " ")
 second_sentence = st.text_input("Second sentence", " ")
 sentences = [first_sentence, second_sentence]
@@ -13,37 +11,41 @@ sentences = [first_sentence, second_sentence]
 option = st.selectbox(
     "Select embedding",
     ("all-MiniLM-L6-v2",
-    "all-mpnet-base-v2",
-    "all-distilroberta-v1",
-    "paraphrase-MiniLM-L6-v2",
-    "paraphrase-mpnet-base-v2",
-    "nli-roberta-base-v2",
-    "stsb-roberta-large",
-    "paraphrase-multilingual-MiniLM-L12-v2",
-    "distiluse-base-multilingual-cased-v1",
-    "sentence-transformers/LaBSE",
-    "paraphrase-multilingual-mpnet-base-v2",
-    "multi-qa-MiniLM-L6-cos-v1",
-    "average_word_embeddings_glove.6B.300d",
-    "msmarco-distilbert-base-v2",
-    "instructor-xl",
-    "e5-base",
-    "bge-small-en-v1.5"))
+        "all-mpnet-base-v2",
+        "all-distilroberta-v1",
+        "paraphrase-MiniLM-L6-v2",
+        "paraphrase-mpnet-base-v2",
+        "nli-roberta-base-v2",
+        "stsb-roberta-large",
+        "paraphrase-multilingual-MiniLM-L12-v2",
+        "distiluse-base-multilingual-cased-v1",
+        "sentence-transformers/LaBSE",
+        "paraphrase-multilingual-mpnet-base-v2",
+        "multi-qa-MiniLM-L6-cos-v1",
+        "average_word_embeddings_glove.6B.300d",
+        "msmarco-distilbert-base-v2",
+        "instructor-xl",
+        "e5-base",
+        "bge-small-en-v1.5"))
 
-option = st.selectbox(
+distance_metric = st.selectbox(
     "Select distance metric",
-    ())
+    ("cosine",
+        "euclidean",
+        "dot",
+        "manhattan",
+        "chebyshev",
+        "minkowski",
+        "angular",
+        "hamming",
+        "jaccard"
+        ))
 
+dm = DistanceMetrics()
 if st.button("Run"):
     model = SentenceTransformer(option)
     embeddings = model.encode(sentences)
-    # print(embeddings.shape)
-    # [3, 384]
-    # 3. Calculate the embedding similarities
-    similarities = model.similarity(embeddings, embeddings)
-    distance = similarities[0][1]
-    # print(similarities)
-
-    st.write("The distance between the embeddings is ", distance.item())
+    distance = dm.calc_according_to_metric(distance_metric, embeddings[0], embeddings[1])
+    st.write("The distance between the embeddings is ", distance)
 
 
