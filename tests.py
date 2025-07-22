ebd87a9ea122da9102b3bf61ff52b5dcf586ce3e
altarh <altarh@mail.tau.ac.il>
2025-07-22 13:23:50 -0400

    nothing

--- tests.py
+++ tests.py
@@ -1,3 +1,10 @@
-import embedding_distances.interface
-
-print(embedding_distances.interface.calculate_distance("hi", "bye"))
+# import json
+# import random
+#
+# with open('data/dev_set.json') as f:
+#     data = json.load(f)
+#
+# sample = random.choice(data)
+# print("Question:", sample['question'])
+# print("Answer:", sample['answer-text'])
+# print("Gold Paragraph:", sample['paragraphs'][0][:300])
